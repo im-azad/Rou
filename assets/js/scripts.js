4b31002148658407bb79b1b9861a55fc13451099
Abul Kalam Azad <me.azad99@gmail.com>
2021-09-20 22:28:52 +0600

    scroll top button added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,3 +10,17 @@ const swiper = new Swiper('.swiper', {
 		clickable: true,
 	},
 });
+
+// Scroll to top button
+const scrollTopBtn = document.querySelector('#scrollTopBtn');
+const rootElement = document.documentElement;
+
+function scrollToTop() {
+	// Scroll to top logic
+	rootElement.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: 'smooth',
+	});
+}
+scrollTopBtn.addEventListener('click', scrollToTop);
